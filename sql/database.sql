d78a4e2354f7c8382d9846d7d07cc12969aab735
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-12-01 14:43:51 +0100

    foo

--- sql/database.sql
+++ sql/database.sql
@@ -1,94 +1,124 @@
--- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: bankwiz_db
--- ------------------------------------------------------
--- Server version	8.0.33
+-- Hôte : mysql
+-- Généré le : ven. 01 déc. 2023 à 13:39
+-- Version du serveur : 8.0.33
+-- Version de PHP : 8.2.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Table structure for table `BANK_ACCOUNT`
+-- Base de données : `bankwiz_db`
 --
 
-DROP TABLE IF EXISTS `BANK_ACCOUNT`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `BANK_ACCOUNT`
+--
+
 CREATE TABLE `BANK_ACCOUNT` (
   `BASE_AMOUNT` int NOT NULL,
-  `GROUP_ID` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ID` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `BANK_ACCOUNT_NAME` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `FK_GROUPRELATEDENTITY_GROUP` (`GROUP_ID`),
-  CONSTRAINT `FK_GROUPRELATEDENTITY_GROUP` FOREIGN KEY (`GROUP_ID`) REFERENCES `USER_GROUP` (`GROUP_ID`)
+  `GROUP_ID` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ID` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `BANK_ACCOUNT_NAME` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `GROUP_RIGHT`
+-- Structure de la table `GROUP_RIGHT`
 --
 
-DROP TABLE IF EXISTS `GROUP_RIGHT`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `GROUP_RIGHT` (
-  `GROUP_ID` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ID` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `USER_ID` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `GROUP_RIGHT` enum('READ','WRITE','ADMIN') COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `FK_GROUP_RIGHT_GROUP` (`GROUP_ID`),
-  KEY `FK_GROUP_RIGHT_USER` (`USER_ID`),
-  CONSTRAINT `FK_GROUP_RIGHT_GROUP` FOREIGN KEY (`GROUP_ID`) REFERENCES `USER_GROUP` (`GROUP_ID`),
-  CONSTRAINT `FK_GROUP_RIGHT_USER` FOREIGN KEY (`USER_ID`) REFERENCES `USER_ACCOUNT` (`ID`)
+  `GROUP_ID` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ID` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `USER_ID` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `GROUP_RIGHT` enum('READ','WRITE','ADMIN') CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `USER_ACCOUNT`
+-- Structure de la table `USER_ACCOUNT`
 --
 
-DROP TABLE IF EXISTS `USER_ACCOUNT`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `USER_ACCOUNT` (
-  `ID` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `AUTH_ID` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `EMAIL` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`ID`)
+  `ID` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `AUTH_ID` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `EMAIL` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `USER_GROUP`
+-- Structure de la table `USER_GROUP`
 --
 
-DROP TABLE IF EXISTS `USER_GROUP`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `USER_GROUP` (
-  `GROUP_ID` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `GROUP_NAME` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  PRIMARY KEY (`GROUP_ID`)
+  `GROUP_ID` varchar(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `GROUP_NAME` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `BANK_ACCOUNT`
+--
+ALTER TABLE `BANK_ACCOUNT`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_GROUPRELATEDENTITY_GROUP` (`GROUP_ID`);
+
+--
+-- Index pour la table `GROUP_RIGHT`
+--
+ALTER TABLE `GROUP_RIGHT`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `FK_GROUP_RIGHT_GROUP` (`GROUP_ID`),
+  ADD KEY `FK_GROUP_RIGHT_USER` (`USER_ID`);
+
+--
+-- Index pour la table `USER_ACCOUNT`
+--
+ALTER TABLE `USER_ACCOUNT`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Index pour la table `USER_GROUP`
+--
+ALTER TABLE `USER_GROUP`
+  ADD PRIMARY KEY (`GROUP_ID`);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `BANK_ACCOUNT`
+--
+ALTER TABLE `BANK_ACCOUNT`
+  ADD CONSTRAINT `FK_GROUPRELATEDENTITY_GROUP` FOREIGN KEY (`GROUP_ID`) REFERENCES `USER_GROUP` (`GROUP_ID`);
+
+--
+-- Contraintes pour la table `GROUP_RIGHT`
+--
+ALTER TABLE `GROUP_RIGHT`
+  ADD CONSTRAINT `FK_GROUP_RIGHT_GROUP` FOREIGN KEY (`GROUP_ID`) REFERENCES `USER_GROUP` (`GROUP_ID`),
+  ADD CONSTRAINT `FK_GROUP_RIGHT_USER` FOREIGN KEY (`USER_ID`) REFERENCES `USER_ACCOUNT` (`ID`);
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-11-26 12:45:38
